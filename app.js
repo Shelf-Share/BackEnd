bd337fab6316f26e6f753fe117c332bef902759e
Achmad Fikri Syofian <91151241+pikriPnyaPemula@users.noreply.github.com>
2024-06-29 15:51:14 +0700

    feature : Set Up folder controller & routes

--- app.js
+++ app.js
@@ -6,7 +6,19 @@ const {PORT} = process.env;
 
 app.use(express.json());
 
+const agencyRouter = require('./routes/agency.routes');
+app.use('/api/v1/agency', agencyRouter);
+
+const authRouter = require('./routes/auth.routes');
+app.use('/api/v1/auth', authRouter);
+
 const categoryRouter = require('./routes/category.routes');
 app.use('/api/v1/category', categoryRouter);
+
+const communityRouter = require('./routes/community.routes');
+app.use('/api/v1/community', communityRouter);
+
+const newsRouter = require('./routes/news.routes');
+app.use('/api/v1/news', newsRouter);
 
 app.listen(PORT, () => console.log('Listening on Port', PORT));
