b05f7e9a5e20005ecfae31f5250f634603a59579
Achmad Fikri Syofian <91151241+pikriPnyaPemula@users.noreply.github.com>
2024-07-04 02:37:05 +0700

    feat: CRUD News, Multer, Imagekit

--- routes/news.routes.js
+++ routes/news.routes.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
-const {getAllNews, getNewsDetails} = require('../controllers/news/news.controller');
+const {getAllNews, getNewsDetails, addNews, updateNews, deleteNews} = require('../controllers/news/news.controller');
+const {image} = require('../libs/multer');
 
 router.get('/', getAllNews);
 router.get('/:id', getNewsDetails);
+router.post('/', image.single('image'), addNews);
+router.put('/:id', image.single('image'), updateNews);
+router.delete('/:id', deleteNews);
 
 module.exports = router;
